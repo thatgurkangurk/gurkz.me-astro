0940324ec4604bb637fdb3ad1a0ad0392a212ce3
Gurkan <hello@gurkz.me>
2023-08-15 17:55:26 +0200

    analytics go brr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import partytown from "@astrojs/partytown";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), partytown()],
+  output: "server",
+  adapter: vercel()
 });
